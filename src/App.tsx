d1b818f456e8cb7f73444a28e96dac4f2c2253da
Varsha Singh <varsha.s@razorpay.com>
2024-10-11 14:44:27 +0530

    feat: set up react routing with all basic website routes

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './components/Homepage'
+import Portfolio from './components/Portfolio'
+import Hobbies from './components/Hobbies'
+import About from './components/About'
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/portfolio" element={<Portfolio/>} />
+            <Route path="/hobbies" element={<Hobbies />} />
+            <Route path="/about" element={<About />}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
