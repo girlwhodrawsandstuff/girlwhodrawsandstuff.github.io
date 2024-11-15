b482c4aebac55613ea05685cd084d6c33adbe50c
Varsha Singh <varsha.s@razorpay.com>
2024-11-15 21:27:44 +0530

    fix: change asset loading path

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -6,9 +6,9 @@ export const Footer = () => {
   return (
     <footer className="px-14 py-6 100dvw flex flex-col items-center justify-center">
       <section className="flex">
-        <Link to='https://www.linkedin.com/in/varshasingh97/'><img className="w-12 cursor-pointer" src="src/shared/assets/linkedin.svg"></img></Link>
-        <Link to='https://github.com/girlwhodrawsandstuff'><img className="w-12 cursor-pointer" src="src/shared/assets/github.svg"></img></Link>
-        <Link to='https://www.instagram.com/madebyvorshe/'><img className="w-12 cursor-pointer" src="src/shared/assets/instagram.svg"></img></Link>
+        <Link to='https://www.linkedin.com/in/varshasingh97/'><img className="w-12 cursor-pointer" src="./src/shared/assets/linkedin.svg"></img></Link>
+        <Link to='https://github.com/girlwhodrawsandstuff'><img className="w-12 cursor-pointer" src="./src/shared/assets/github.svg"></img></Link>
+        <Link to='https://www.instagram.com/madebyvorshe/'><img className="w-12 cursor-pointer" src="./src/shared/assets/instagram.svg"></img></Link>
       </section>
       <small className="pt-3">©Varsha Singh {currentYear}</small>
     </footer>
