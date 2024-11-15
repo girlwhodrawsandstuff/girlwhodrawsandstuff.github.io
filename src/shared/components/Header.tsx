0a946f427c24e637687de7f1664e86afd464c87a
Varsha Singh <varsha.s@razorpay.com>
2024-11-15 20:29:50 +0530

    feat: add header categories and extract into config

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,5 +1,15 @@
+import { ReactNode } from "react";
+import { appConfig } from "../configs/config";
+
 export const Header = () => {
-    return <header>
-        this is the header
+  const { categories } = appConfig.header;
+
+  return (
+    <header>
+      <a>LOGO</a>
+      {categories.map((category): ReactNode => {
+        return <a key={category}>{category}</a>
+      })}
     </header>
+  );
 };
--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -1,5 +1,5 @@
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return <div>I'm currently updating my site. Please check back soon.</div>;
 };
 
 export default Homepage;
