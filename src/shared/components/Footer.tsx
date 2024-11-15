c19a492bb571e45f2f30c7402c46ccc224d73567
Varsha Singh <varsha.s@razorpay.com>
2024-11-15 22:05:30 +0530

    fix: change asset loading path

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import linkedinIcon from '../../assets/linkedin.svg';
+import githubIcon from '../../assets/linkedin.svg';
+import instagramIcon from '../../assets/linkedin.svg';
 
 export const Footer = () => {
-  // get current year
   const currentYear = new Date().getFullYear();
   return (
     <footer className="px-14 py-6 100dvw flex flex-col items-center justify-center">
       <section className="flex">
-        <Link to='https://www.linkedin.com/in/varshasingh97/'><img className="w-12 cursor-pointer" src="src/shared/assets/linkedin.svg"></img></Link>
-        <Link to='https://github.com/girlwhodrawsandstuff'><img className="w-12 cursor-pointer" src="src/shared/assets/github.svg"></img></Link>
-        <Link to='https://www.instagram.com/madebyvorshe/'><img className="w-12 cursor-pointer" src="src/shared/assets/instagram.svg"></img></Link>
+        <Link to='https://www.linkedin.com/in/varshasingh97/'><img className="w-12 cursor-pointer" src={linkedinIcon}></img></Link>
+        <Link to='https://github.com/girlwhodrawsandstuff'><img className="w-12 cursor-pointer" src={githubIcon}></img></Link>
+        <Link to='https://www.instagram.com/madebyvorshe/'><img className="w-12 cursor-pointer" src={instagramIcon}></img></Link>
       </section>
       <small className="pt-3">©Varsha Singh {currentYear}</small>
     </footer>
