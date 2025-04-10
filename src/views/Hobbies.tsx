aafdf7d0b60d52a11433c85f7c5d1b87d3d18b8f
Varsha Singh <varsha.s@razorpay.com>
2025-04-10 16:42:56 +0530

    feat: add content for hobbies page

--- src/views/Hobbies.tsx
+++ src/views/Hobbies.tsx
@@ -1,5 +1,57 @@
+import crochetImg from '../assets/hobbies/crochet-knitting.webp';
+import polymerClayImg from '../assets/hobbies/polymer-clay.webp';
+import paintingImg from '../assets/hobbies/painting.webp';
+import bakingImg from '../assets/hobbies/baking.webp';
+import boardGamesImg from '../assets/hobbies/board-games.webp';
+import gamingImg from '../assets/hobbies/gaming.webp';
+
 const Hobbies = () => {
-  return <div className="px-14 py-6">Hobbies</div>;
+  const hobbiesList = [
+    {
+      title: 'Crochet & Knitting',
+      image: crochetImg,
+      desc: 'I love creating everything from tiny animals to huge blankets using colorful yarn!',
+    },
+    {
+      title: 'Polymer Clay',
+      image: polymerClayImg,
+      desc: 'Making miniature food, trinkets, and cat-themed charms is my jam.',
+    },
+    {
+      title: 'Painting & Art',
+      image: paintingImg,
+      desc: "I'm not particularly good at painting fancy things, but I love it.",
+    },
+    {
+      title: 'Baking',
+      image: bakingImg,
+      desc: "I love baking cakes and cookies. Can't promise that they'll taste good though.",
+    },
+    {
+      title: 'Board Games',
+      image: boardGamesImg,
+      desc: "I love playing board games with my friends. Some of my favorite board games include Takenoko, Sheriff of Nottingham, Skulls, and Azul. And yes, I've played Catan too. Lately, I also love Dungeons & Dragons — still a noob, but very committed to the lore and drama.",
+    },
+    {
+      title: 'Gaming',
+      image: gamingImg,
+      desc: 'I play chill games like The Sims 4, Minecraft and also really chaotic games like Elden Ring. I love games that let me get lost in a world. My all-time favorite is The Witcher 3: Wild Hunt. Gaming is where I unwind, recharge, and sometimes scream at my screen.',
+    },
+  ];
+  return (
+    <>
+      <h1>Hobbies</h1>
+      {hobbiesList.map((hobby, index) => (
+        <article key={index}>
+          <img src={hobby.image} alt={hobby.title} />
+          <div>
+            <h2>{hobby.title}</h2>
+            <p>{hobby.desc}</p>
+          </div>
+        </article>
+      ))}
+    </>
+  );
 };
 
 export default Hobbies;
