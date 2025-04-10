144a62ea7b44aa31e63ddfec26e191adc37ecd3b
Varsha Singh <varsha.s@razorpay.com>
2025-04-10 16:44:31 +0530

    feat: add fallback content for portfolio

--- src/views/Portfolio.tsx
+++ src/views/Portfolio.tsx
@@ -1,5 +1,9 @@
 const Portfolio = () => {
-  return <div className="px-14 py-6">Portfolio</div>;
+  return (
+    <div className="px-14 py-6">
+      Nothing to see here yet. I'll be listing some projects shortly.
+    </div>
+  );
 };
 
 export default Portfolio;
