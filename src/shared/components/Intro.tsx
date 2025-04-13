32ef95adcee64e3da2421ac145d12607c3d817fd
Varsha Singh <varsha.s@razorpay.com>
2025-04-13 11:39:24 +0530

    feat: update intro content

--- src/shared/components/Intro.tsx
+++ src/shared/components/Intro.tsx
@@ -1,29 +1,31 @@
 const Intro = () => {
   return (
     <>
-      <h1>Hi, I'm Varsha!</h1>
-      <p>
-        <p>Frontend Engineer at Razorpay, based in Bangalore, India.</p>
-      </p>
-
-      <article>
+      <section>
+        <h1>Hi, I'm Varsha!</h1>
         <p>
-          I build performant, scalable web applications using JavaScript, React, and Angular —
-          writing code that goes beyond the UI. I enjoy solving complex problems, architecting
-          maintainable frontends, and obsessing over the details that make apps fast, accessible,
-          and reliable.
+          <p>Frontend Engineer at Razorpay, based in Bangalore, India.</p>
         </p>
 
-        <p>
-          Lately, I've been exploring machine learning and generative AI which I can leverage as
-          powerful tools across development workflows, from documentation to testing and beyond.
-        </p>
+        <article>
+          <p>
+            I build performant, scalable web applications using JavaScript, React, and Angular —
+            writing code that goes beyond the UI. I enjoy solving complex problems, architecting
+            maintainable frontends, and obsessing over the details that make apps fast, accessible,
+            and reliable.
+          </p>
 
-        <p>
-          When I'm not coding, I'm probably making something cutsie with polymer clay, crocheting,
-          or befriending every cat and dog I meet!
-        </p>
-      </article>
+          <p>
+            Lately, I've been exploring machine learning and generative AI which I can leverage as
+            powerful tools across development workflows, from documentation to testing and beyond.
+          </p>
+
+          <p>
+            When I'm not coding, I'm probably making something cutsie with polymer clay, crocheting,
+            or befriending every cat and dog I meet!
+          </p>
+        </article>
+      </section>
     </>
   );
 };
