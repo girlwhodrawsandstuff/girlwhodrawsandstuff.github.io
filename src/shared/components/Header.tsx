a5b0aab24a01e45fc420905b4a044090a74e221c
Varsha Singh <varsha.s@razorpay.com>
2025-08-13 14:03:37 +0530

    style: add styling to header mobile and desktop

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,17 +1,31 @@
-import { ReactNode } from "react";
-import { appConfig } from "../configs/config";
-import { Link } from "react-router-dom";
+import { ReactNode } from 'react';
+import { appConfig } from '../configs/config';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   const { categories } = appConfig.header;
 
   return (
-    <header className="w-dvw flex items-center justify-between px-14 py-6">
-      <a href="/" className="cursor-pointer">LOGO</a>
-      <div className="flex items-center justify-evenly">
-      {categories.map((category): ReactNode => {
-        return <Link key={category} className="cursor-pointer px-5" to={category.toLowerCase()}>{category}</Link>
-      })}
+    <header className="w-dvw flex items-center justify-between lg:px-14 lg:py-6 py-4 px-3">
+      <a href="/" className="cursor-pointer flex items-center gap-1">
+        <img src="src/assets/code.svg" alt="code icon" className="w-6" />
+        <p className="font-mono text-stone-700 font-medium">
+          girl<span className="text-stone-500">who</span>draws
+          <span className="text-stone-500">and</span>stuff
+        </p>
+      </a>
+      <div className="flex items-center justify-evenly hidden md:block">
+        {categories.map((category): ReactNode => {
+          return (
+            <Link
+              key={category}
+              className="cursor-pointer px-5 text-stone-600 hover:text-stone-800"
+              to={category.toLowerCase()}
+            >
+              {category}
+            </Link>
+          );
+        })}
       </div>
     </header>
   );
